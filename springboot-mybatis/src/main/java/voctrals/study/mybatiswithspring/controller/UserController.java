917747ed1f75a51ac7d1934578224e48573a7827
刘磊 <liul@htdadao.net>
2018-04-10 18:25:25 +0800

    with-spring

---
+++ springboot-mybatis/src/main/java/voctrals/study/mybatiswithspring/controller/UserController.java
@@ -0,0 +1,69 @@
+package voctrals.study.mybatiswithspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import voctrals.study.mybatiswithspring.domain.User;
+import voctrals.study.mybatiswithspring.service.UserService;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(){
+		return "hello";
+	}
+	/**
+	 * 测试插入
+	 * @return
+	 */
+	@RequestMapping("/add")
+	@ResponseBody
+	public String add(){
+		User u = new User();
+		double i = Math.random()*100;
+		u.setId(String.valueOf(i));
+		u.setUserName("test"+i);
+		this.userService.insertUser(u);
+		return "success";
+	}
+	/**
+	 * 测试分页插件
+	 * @return
+	 */
+	@RequestMapping("/queryPage")
+	@ResponseBody
+	public String queryPage(){
+		PageInfo<User> page = this.userService.queryPage("tes", 1, 2);
+		System.out.println("总页数=" + page.getPages());
+		System.out.println("总记录数=" + page.getTotal()) ;
+		for(User u : page.getList()){
+			System.out.println(u.getId() + " \t " + u.getUserName());
+		}
+		return "success";
+	}
+	/**
+	 * 测试事务
+	 * @return
+	 */
+	@RequestMapping("/testTransational")
+	@ResponseBody
+	public String test(){
+		try {
+			this.userService.testTransational();
+			return "success";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+		
+	}
+}
---
+++ springboot-mybatis/src/main/java/voctrals/study/mybatiswithspring/dao/UserMapper.java
@@ -0,0 +1,23 @@
+package voctrals.study.mybatiswithspring.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import voctrals.study.mybatiswithspring.domain.User;
+
+@Mapper
+public interface UserMapper {
+
+	@Insert("insert sys_user(id,user_name) values(#{id},#{userName})")
+	void insert(User u);
+	
+	//注：方法名和要UserMapper.xml中的id一致
+	List<User> query(@Param("userName")String userName);
+	
+	@Delete("delete from sys_user")
+	void deleteAll();
+}
